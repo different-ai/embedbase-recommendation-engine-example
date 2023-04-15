f0ce74894c67a8c36b02d974096cf6676f3745a3
Louis Beaumont <louis.beaumont@gmail.com>
2023-04-15 09:34:19 +0200

    chore: blank slate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/posts/understanding-machine-learning-embeddings')
-  }, [router])
   return (
     <>
       <Head>
